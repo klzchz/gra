d3b866c656f7682919d70a9d21d00207596a9e41
Lucas Andrade <lucascorporation@live.com>
2025-07-10 00:08:43 -0300

    database memoria

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,9 @@
-
 import { Sequelize } from "sequelize";
-import path from "path";
-
-
-const storage = path.join(__dirname, "..", "..", "database.sqlite");
-
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: storage, 
-});
-
-const sequelizeTest = new Sequelize("sqlite::memory:", {
+  storage: ":memory:",
   logging: false,
 });
 
-
-export default process.env.NODE_ENV === "test" ? sequelizeTest : sequelize;
+export default sequelize;
